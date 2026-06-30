d4dcc71061580766afc669252c8bf3c3fb0fd178
Aliksalot <alexkolev05@gmail.com>
2026-06-30 22:03:40 +0300

    A lot of changes to c++ part

---
+++ cpp/darray.h
@@ -0,0 +1,327 @@
+#pragma once
+
+#include<cstdlib>
+#include<stdexcept>
+
+namespace clib {
+
+  template<typename T>
+  class darray {
+
+    public:
+      /*Since move_if_noexcept is forbidden, convetion
+       *is that moving objects is well-defined and is noexcept for all types.*/
+      darray();
+      darray(const T* arr, std::size_t len);
+      ~darray();
+
+      darray& add(const T& item);
+
+      darray(const darray& l);
+      darray& operator=(const darray& l);
+      darray(darray&& l) noexcept;
+      darray& operator=(darray&& l) noexcept;
+
+      T& operator[](std::size_t index);
+      const T& operator[](std::size_t index) const;
+
+      void extend(const darray<T>& list);
+      void clear();
+      darray& remove(std::size_t st, std::size_t items = 1);
+      darray& insert(std::size_t at, const T& item);
+      T pop();
+      bool contains(const T& item) const;
+      std::size_t size() const;
+      long indexOf(const T& item) const;
+      bool empty() const;
+      
+      T* begin();
+      T* end();
+
+      const T* begin() const;
+      const T* end() const;
+
+    private:
+      T* data = nullptr;
+      std::size_t count = 0;
+      std::size_t cap = 0;
+
+      void expand();
+      void reserve(std::size_t space);
+      bool indexInBounds(std::size_t index) const;
+      bool full() const;
+
+      /**Destroys all objects in data, starting from std::size_t st
+       * and sets count to 0. DOESN'T free memory, allocated in data[] */
+      void clear_data(std::size_t st = 0);
+      /**Calls delete on data. Does NOT destruct T objects, only frees memory*/
+      void delete_data();
+      /**
+       * Allocates memory and stores address in data pointer.
+       * Doesn't call T constructor.
+       * */
+      T* construct_data(std::size_t size);
+  };
+
+  //-------DECLARATIONS------
+
+  template<typename T>
+  void darray<T>::clear() {
+    clear_data();
+  }
+
+  template<typename T>
+  void darray<T>::delete_data() {
+    ::operator delete(data);
+    data = nullptr;
+  }
+
+  template<typename T>
+  T* darray<T>::construct_data(std::size_t size) {
+    return static_cast<T*>(::operator new(sizeof(T) * size));
+  }
+  template<typename T>
+  void darray<T>::clear_data(std::size_t st) {
+    if(st >= count) return;
+
+    for(std::size_t i = st; i < count; i ++) {
+      data[i].~T();
+    }
+    count = st;
+  }
+
+  template<typename T>
+  void darray<T>::reserve(std::size_t space) {
+
+    if(space <= cap) return;
+
+    T* temp = construct_data(space);
+    std::size_t tempCount = count;
+
+    for(std::size_t i = 0; i < count; i ++) {
+      new (temp + i) T(static_cast<T&&>(data[i]));
+    }
+
+    clear_data();
+    delete_data();
+
+    data = temp;
+    count = tempCount;
+    cap = space;
+  }
+
+  template<typename T>
+  void darray<T>::expand() {
+    reserve(cap == 0 ? 1 : cap * 2);
+  }
+
+  template<typename T>
+  darray<T>::darray(const T* arr, std::size_t len) {
+    reserve(len);
+    
+    for(std::size_t i = 0; i < len; i ++) {
+      new (data + i) T(arr[i]);
+    }
+
+    count = len;
+  }
+
+  template<typename T>
+  darray<T>::darray() {}
+
+  template<typename T>
+  darray<T>::~darray() {
+    clear_data();
+    delete_data();
+  }
+
+  template<typename T>
+  darray<T>& darray<T>::add(const T& item) {
+    if(full()) expand();
+
+    new (data + count) T(item);
+    count ++; 
+
+    return *this;
+  }
+
+  template<typename T>
+  void darray<T>::extend(const darray<T>& list) {
+
+    reserve(count + list.size());
+    std::size_t initialCount = count;
+    try{
+      for(std::size_t i = 0; i < list.size(); i ++){
+        new (data + count) T(list[i]);
+        count ++;
+      }
+    }catch(...) {
+      clear_data(initialCount);
+      throw;
+    }
+   };
+
+  template<typename T>
+  darray<T>::darray(const darray& l) {
+    reserve(l.cap);
+    count = l.count;
+    for(std::size_t i = 0; i < l.count; i ++) {
+      new (data + i) T(l.data[i]);
+    }
+  }
+  template<typename T>
+  darray<T>& darray<T>::operator=(const darray& l) {
+    if(&l == this) return *this;
+
+    clear_data();
+    reserve(l.cap);
+
+    for(std::size_t i = 0; i < l.count; i ++) {
+      new (data + i) T(l.data[i]);
+      count ++;
+    }
+    return *this;
+  }
+
+  template<typename T>
+  darray<T>::darray(darray&& l) noexcept{
+    data = l.data;
+    count = l.count;
+    cap = l.cap;
+    l.data = nullptr;
+    l.cap = 0;
+    l.count = 0;
+  }
+
+  template<typename T>
+  darray<T>& darray<T>::remove(std::size_t st, std::size_t items) {
+    if(st >= count || items == 0 || items > count - st) return *this;
+    
+    for(std::size_t i = st; i < count - items; i ++) {
+      data[i] = static_cast<T&&>(data[items + i]);
+    }
+
+    clear_data(count - items);
+    return *this;
+  }
+
+  template<typename T>
+  darray<T>& darray<T>::insert(std::size_t at, const T& item) { 
+    if(at == count) return add(item);
+
+    if(!indexInBounds(at)) throw std::out_of_range("insert: index out of bounds");
+
+    if(full()) expand();
+    
+    new (data + count) T(static_cast<T&&>(data[count - 1]));
+    for(std::size_t i = count - 1; i > at; i --) {
+      data[i] = static_cast<T&&>(data[i-1]);
+    }
+    data[at] = item;
+    count ++;
+
+    return *this;
+  }
+
+  template<typename T>
+    T darray<T>::pop() {
+      if(count == 0)
+        throw std::out_of_range("list pop: can't pop on an empty list");
+
+      T temp = static_cast<T&&>(data[count - 1]);
+      clear_data(count - 1);
+
+      return temp;
+    }
+
+  template<typename T>
+  long darray<T>::indexOf(const T& item) const {
+    for(std::size_t i = 0; i < count; i ++) {
+      if(data[i] == item) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  template<typename T>
+  darray<T>& darray<T>::operator=(darray&& l) noexcept{
+    if(this == &l) return *this;
+
+    clear_data();
+    delete_data();
+
+    data = l.data;
+    count = l.count;
+    cap = l.cap;
+
+    l.data = nullptr;
+    l.cap = 0;
+    l.count = 0;
+
+    return *this;
+  }
+
+  template<typename T>
+  T& darray<T>::operator[](std::size_t index) {
+    if(indexInBounds(index)) {
+        return data[index];
+    }
+    throw std::out_of_range("index out of bounds");
+  }
+
+  template<typename T>
+  const T& darray<T>::operator[](std::size_t index) const{
+    if(indexInBounds(index)) {
+        return data[index];
+    }
+    throw std::out_of_range("index out of bounds");
+  }
+
+
+  template<typename T>
+  bool darray<T>::contains(const T& item) const {
+    for(std::size_t i = 0; i < count; i ++) {
+      if(data[i] == item) return true;
+    }
+    return false;
+  }
+
+  template<typename T>
+  std::size_t darray<T>::size() const{
+    return count;
+  }
+
+  template<typename T>
+  bool darray<T>::indexInBounds(std::size_t index) const {
+    return index < count;
+  }
+
+  template<typename T>
+  bool darray<T>::full() const {
+    return cap == count;
+  }
+
+  template<typename T>
+  bool darray<T>::empty() const {
+    return count == 0;
+  }
+  template<typename T>
+  T* darray<T>::begin() {
+    return data;
+  }
+  template<typename T>
+  T* darray<T>::end() {
+    return data + count;
+  }
+  template<typename T>
+  const T* darray<T>::begin() const {
+    return data;
+  }
+  template<typename T>
+  const T* darray<T>::end() const {
+    return data + count;
+  }
+
+}
+
---
+++ cpp/mystring.h
@@ -0,0 +1,254 @@
+#pragma once
+
+#include"./darray.h"
+#include<cstddef>
+#include<stdexcept>
+#include<cstring>
+#include<ostream>
+#include<istream>
+
+namespace clib{
+
+  class Text {
+  public:
+    Text();
+    Text(const char* s);
+    Text(const char* s, std::size_t len);
+
+    const char& operator[](std::size_t index) const;
+    char& operator[](std::size_t index);
+    Text& operator+=(const Text& s);
+    Text& operator+=(char c);
+    Text& operator+=(const char* s);
+    Text& operator=(const char* s);
+    bool operator==(const Text& s) const;
+    bool operator==(const char* s) const;
+    bool operator!=(const Text& s) const;
+    bool operator!=(const char* s) const;
+
+    long long toInt() const;
+    static Text fromInt(long long i);
+    bool isValidInt() const;
+
+    Text& insert(std::size_t at, char c);
+    Text trim(char c = ' ') const;
+    Text& trimInPlace(char c = ' ');
+
+    darray<Text> split(char c) const;
+
+    bool empty() const;
+
+    void clear();
+    std::size_t size() const;
+
+    const char* raw() const;
+
+  private:
+    darray<char> l;
+  };
+
+  inline Text& Text::trimInPlace(char c) {
+    l.pop();
+
+    while(!l.empty() && l[0] == c) {
+      l.remove(0);
+    }
+
+    while(!l.empty() && l[l.size() - 1] == c) {
+      l.remove(l.size() - 1);
+    }
+
+    l.add('\0');
+    return *this;
+  }
+
+  inline Text Text::trim(char c) const {
+    Text trimmed(*this);
+    return trimmed.trimInPlace(c);
+  }
+
+  inline void Text::clear() {
+    l.clear();
+    l.add('\0');
+  }
+  inline std::size_t Text::size() const {
+    return l.size() - 1;
+  }
+
+  inline Text::Text() { l.add('\0'); };
+  inline Text::Text(const char* s): Text(s, std::strlen(s)) { };
+  inline Text::Text(const char* s, std::size_t len) {
+    l = darray<char>(s, len);
+    l.add('\0');
+  }
+
+  inline bool Text::empty() const {
+    return size() == 0;
+  }
+
+  inline Text& Text::operator=(const char* s) {
+    l = darray<char>(s, std::strlen(s));
+    l.add('\0');
+    return *this;
+  }
+  inline char& Text::operator[](std::size_t index) {
+    return l[index];
+  }
+
+  inline const char& Text::operator[](std::size_t index) const{
+    return l[index];
+  }
+  inline Text& Text::operator+=(const Text& s) {
+    if(this == &s) {
+      Text copy = s;
+      l.pop();
+      l.extend(copy.l);
+      return *this;
+    }
+    l.pop();
+    l.extend(s.l);
+    return *this;
+  }
+  inline Text& Text::operator+=(char c) {
+    l.pop();
+    l.add(c);
+    l.add('\0');
+    return *this;
+  }
+  inline Text& Text::operator+=(const char* s) {
+    return *this += Text(s);
+  }
+  inline Text operator+(Text a, const Text& b) {
+    return a += b;
+  }
+  inline Text operator+(Text a, long long n) {
+    return a += Text::fromInt(n);
+  }
+  inline bool Text::operator==(const Text& s) const {
+    if(size() != s.size()) return false;
+
+    for(std::size_t i = 0; i < size(); i ++) {
+      if(l[i] != s[i]) return false;
+    }
+    return true;
+  }
+  inline bool Text::operator==(const char* s) const {
+    return *this == Text(s);
+  }
+
+  inline bool Text::operator!=(const Text& s) const {
+    return !(*this == s);
+  }
+  inline bool Text::operator!=(const char* s) const {
+    return !(*this == s);
+  }
+
+  inline bool Text::isValidInt() const {
+    if(size() == 0 || (size() == 1 && l[0] == '-')) return false;
+    for(std::size_t i = 0; i < size(); i ++) {
+      if(l[i] < '0' || l[i] > '9') return false;
+    }
+    return true;
+  }
+
+  inline long long Text::toInt() const {
+    if(!isValidInt())
+      throw std::invalid_argument("Not an integer");
+    
+    signed sign = 1;
+    std::size_t i = 0;
+
+    if(l[0] == '-') {
+      i = 1;
+      sign = -1;
+    }
+
+    long long r = 0;
+    for(; i < size(); i ++) {
+      r = r * 10 + (l[i] - '0');
+    }
+
+    return r * sign;
+  }
+
+  inline Text Text::fromInt(long long i) {
+    if(i == 0) return "0";
+
+    Text r;
+    bool neg = i < 0;
+    if(neg) i = -i;
+
+    while(i > 0) {
+      char c = (i % 10) + '0';
+      r.insert(0, c);
+      i /= 10;
+    }
+
+    if(neg) r.insert(0, '-');
+
+    return r;
+  }
+
+  inline darray<Text> Text::split(char c) const {
+    darray<Text> out;
+    Text temp;
+    std::size_t stringLength = size();
+    for(std::size_t i = 0; i < stringLength; i ++) {
+      if(l[i] == c) {
+        out.add(temp);
+        temp = "";
+        continue;
+      }
+
+      temp += l[i];
+    }
+    out.add(temp);
+    return out;
+  }
+
+  inline Text& Text::insert(std::size_t at, char c) {
+    l.insert(at, c);
+    return *this;
+  }
+
+  inline std::ostream& operator<<(std::ostream& stream, const Text& s) {
+    for(std::size_t i = 0; i < s.size(); i ++) {
+      stream << s[i];
+    }
+    return stream;
+  }
+
+  inline bool operator==(const char* sraw, const Text& s) {
+    return s == sraw;
+  }
+
+  inline std::istream& operator>>(std::istream& stream, Text& s) {
+    s.clear();
+
+    char c;
+    while(stream.get(c)) {
+      if(c == ' ' || c == '\n' || c == '\t') break;
+
+      s += c;
+    }
+
+    return stream;
+  }
+
+  inline const char* Text::raw() const{
+    return l.size() ? &l[0] : "";
+  }
+
+  inline std::istream& getLine(std::istream& stream, clib::Text& s, char delim = '\n') {
+    s.clear();
+    char c;
+    while(stream.get(c)) {
+        if(c == delim) break;
+        s += c;
+    }
+    return stream;
+  }
+
+
+}
+
